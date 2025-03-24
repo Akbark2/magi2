0dfa2f2626098e7070a136b131f628e5adfeb650
Akbark2 <hajiakbarblloch@gmail.com>
2025-03-24 21:20:47 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+$title = "Magis TV – Canales en directo y HD, filmes, series, deportes";
+
+// Add the meta tags for Google indexing and verification
+echo "<head>
+    <meta name='robots' content='index, follow' /> <!-- This is the robo tag for Google indexing -->
+    <meta name='google-site-verification' content='NXPj3aLMxGA5iY0xeahfQri22ZBuLW8Zl_EfqWejHUA' /> <!-- Verification code -->
+</head>";
+
+echo "<h1>$title</h1>
+<p>Magis TV ofrece más de 1300 canales en directo y HD, junto con más de 50,000 series y películas. También cuenta con más de 250 canales de deportes y programas a la carta en todas tus pantallas con una señal estable.</p>
+
+<p>Consigue un streaming rápido con la posibilidad de elegir una calidad de video de hasta 4K Ultra HD, además de la opción de ver programas en varios idiomas y contenido deportivo de primer nivel con un diseño intuitivo. Magis TV Apk garantiza una emisión sin interrupciones en smartphones, tabletas, Android TV y más.</p>
+
+<h2>Cómo descargar e instalar Magis TV gratis</h2>
+<p>Dado que Magis TV Apk no está disponible en Google Play Store, puedes descargarlo desde nuestro sitio web de forma gratuita haciendo clic en el siguiente enlace:</p>
+<p><a href='https://themagistv.com.co' target='_blank'>Magis TV Apk</a></p>
+
+<h2>Características de Magis TV</h2>
+<ul>
+<li>Más de 1300 canales de televisión</li>
+<li>Sección dedicada a los niños</li>
+<li>Streaming en HD y 4K</li>
+<li>Películas, series, deportes y más</li>
+<li>Compatible con varios dispositivos</li>
+<li>Soporte multilingüe</li>
+<li>Control parental</li>
+<li>Actualizaciones periódicas</li>
+</ul>
+
+<h2>Planes y Precios</h2>
+<p>Magis TV Colombia ofrece varios planes de suscripción:</p>
+<ul>
+<li>1 Mes: $9</li>
+<li>3 Meses: $27</li>
+<li>6 Meses + 1 Mes Gratis: $49</li>
+<li>12 Meses + 2 Meses Gratis: $87</li>
+</ul>
+
+<h2>Descargar Magis TV para PC</h2>
+<p>Magis TV no tiene una versión oficial para PC, pero puedes utilizar un emulador como BlueStacks para ejecutarlo en tu ordenador.</p>
+
+<h2>Enlace al sitio oficial</h2>
+<p>Si deseas más información, visita el <a href='https://themagistv.com.co' target='_blank'>sitio oficial de Magis TV</a>.</p>
+
+<h2>Otros Enlaces:</h2>
+<ul>
+    <li><a href='https://magistv-68e4f8.gitlab.io/' target='_blank'>https://magistv-68e4f8.gitlab.io/</a></li>
+    <li><a href='https://f005.backblazeb2.com/file/Magi01/index.html' target='_blank'>https://f005.backblazeb2.com/file/Magi01/index.html</a></li>
+    <li><a href='https://magistv0.netlify.app/' target='_blank'>https://magistv0.netlify.app/</a></li>
+    <li><a href='https://magi-woad.vercel.app/' target='_blank'>https://magi-woad.vercel.app/</a></li>
+    <li><a href='https://magi-qzrj.onrender.com' target='_blank'>https://magi-qzrj.onrender.com</a></li>
+</ul>";
+?>
